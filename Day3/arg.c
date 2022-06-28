acea7fe0f48ede2fc86253bd1984f3d231bdafa6
rudfo3264 <rudfo3264@naver.com>
2022-06-28 17:34:48 +0900

    Day3 TCP IP 통신

---
+++ Day3/arg.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	// 시간복잡도 n
+	int n;
+	int i;
+	int result = 0;
+	printf("숫자 입력 : ");
+	scanf("%d",&n);
+
+	//for(i=1; i<=n; i++)
+	//{
+	//	result = result+i;
+	//}
+
+	//시간 복잡도 n/2
+	for(i=0; i <= n; i++)
+
+
+	printf("%d\n",result);
+
+	return 0;
+}
